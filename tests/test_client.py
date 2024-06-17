828ace02f6462e463579b03547ff701523075dff
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-06-17 21:24:22 +0530

    test: create test_client for get_version

---
+++ tests/test_client.py
@@ -0,0 +1,10 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from aria2py import Aria2Client
+
+aria2 = Aria2Client("localhost", 6800)
+
+def test_get_version():
+    assert 'version' in aria2.get_version(raw=True)["result"], "Expected key 'version' in response. Check connection/credentials."
+    assert isinstance(aria2.get_version(), str), "Expected string response. Check server connection/credentials."
