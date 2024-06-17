f5640c3a8f88f51f2d94b8ec436c0dcac82d48e3
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-06-17 20:55:26 +0530

    refactor: updated imports

--- aria2py/__init__.py
+++ aria2py/__init__.py
@@ -1,2 +1,4 @@
-__version__ = "0.1.0"
-__author__ = "Rudransh Joshi (FireHead90544)"
+from .client import Aria2Client
+
+__version__: str = "0.1.0"
+__author__: str = "Rudransh Joshi (FireHead90544)"
